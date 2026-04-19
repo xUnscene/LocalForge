cb7d8dd56954701e4843b5d50cc65f7c9664e5c8
Jon Eatmon <jon.eatmon@gmail.com>
2026-04-19 05:28:45 -0600

    feat: add contextBridge IPC with db:getAllGenerations handler

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,22 +1,15 @@
-import { contextBridge } from 'electron'
-import { electronAPI } from '@electron-toolkit/preload'
+import { contextBridge, ipcRenderer } from 'electron'
 
-// Custom APIs for renderer
-const api = {}
-
-// Use `contextBridge` APIs to expose Electron APIs to
-// renderer only if context isolation is enabled, otherwise
-// just add to the DOM global.
-if (process.contextIsolated) {
-  try {
-    contextBridge.exposeInMainWorld('electron', electronAPI)
-    contextBridge.exposeInMainWorld('api', api)
-  } catch (error) {
-    console.error(error)
+export const api = {
+  db: {
+    getAllGenerations: () => ipcRenderer.invoke('db:getAllGenerations'),
   }
-} else {
-  // @ts-ignore (define in dts)
-  window.electron = electronAPI
-  // @ts-ignore (define in dts)
-  window.api = api
+}
+
+contextBridge.exposeInMainWorld('localforge', api)
+
+declare global {
+  interface Window {
+    localforge: typeof api
+  }
 }
